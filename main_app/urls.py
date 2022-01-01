72c7a2c3beed43d55143b4baa3adc808dcb4d07f
breyshaw <dawsonbrey@gmail.com>
2022-01-01 12:53:22 -0500

    create mask index functionality, link up bootstrap, create seperate css files

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('masks/', views.masks_index, name='masks_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 # For testing with the templates, may keep this page...
 def about(request):
   return render(request, 'about.html')
+
+
+# Simulating Masks for testing purposes
+class Mask:
+  def __init__(self, name, details, guide):
+    self.name = name
+    self.details = details
+    self.guide = guide
+
+masks = [
+  Mask('Deku Mask', 'Change into the body of a deceased Deku','Finish the first three days..'),
+  Mask('Zora Mask', 'Change into the body of a deceased Zora','Play the song of healing for the zora at the great bay'),
+  Mask('Goron Mask', 'Change into the body of a deceased Goron','Play the song of healing for the Goron in Snowhead')
+]
+
+def masks_index(request):
+  return render(request, 'masks/index.html', { 'masks': masks })
