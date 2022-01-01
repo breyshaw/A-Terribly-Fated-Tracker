4465ac30cec6d75253d9e4a17b6a39d9077fa657
breyshaw <dawsonbrey@gmail.com>
2022-01-01 14:48:43 -0500

    create Mask model, render created mask on mask index view, remove testing class from views

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Mask(models.Model):
+  name = models.CharField(max_length=100)
+  details = models.TextField(max_length=250)
+  guide = models.CharField(max_length=500)
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Mask
 
 def home(request):
-  return HttpResponse('Home view works!')
+  return render(request, 'home.html')
 
 # For testing with the templates, may keep this page...
 def about(request):
   return render(request, 'about.html')
 
-
-# Simulating Masks for testing purposes
-class Mask:
-  def __init__(self, name, details, guide):
-    self.name = name
-    self.details = details
-    self.guide = guide
-
-masks = [
-  Mask('Deku Mask', 'Change into the body of a deceased Deku','Finish the first three days..'),
-  Mask('Zora Mask', 'Change into the body of a deceased Zora','Play the song of healing for the zora at the great bay'),
-  Mask('Goron Mask', 'Change into the body of a deceased Goron','Play the song of healing for the Goron in Snowhead')
-]
-
 def masks_index(request):
+  masks = Mask.objects.all()
   return render(request, 'masks/index.html', { 'masks': masks })
